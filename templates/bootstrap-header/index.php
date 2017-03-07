201998e660a940a58d4c40fde2d7e4df95beeaff
KinagaCMS <info@xn--v8jtdudb.com>
2017-03-07 12:11:11 +0900

    Add files via upload

---
+++ templates/bootstrap-header/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang=<?=$lang?>>
+<head>
+<meta charset=<?=$encoding?>>
+<meta name=viewport content="width=device-width,initial-scale=1">
+<link href="<?=$css?>" rel=stylesheet>
+<?=$header?>
+</head>
+<body id=TOP>
+<header>
+<div class=container>
+<div class=row>
+<div class=col-xs-4><a href="<?=$url?>"><?=$site_name?></a></div>
+<div class="col-xs-8 text-right"><?=$meta_description?></div>
+</div>
+</div>
+</header>
+<nav class="navbar navbar-inverse">
+<div class=container>
+<div class=navbar-header>
+<button type=button class=navbar-toggle data-toggle=collapse data-target=#navbar tabindex=9 accesskey=t>
+<span class=icon-bar></span>
+<span class=icon-bar></span>
+<span class=icon-bar></span>
+<span class=sr-only>&#9776;</span>
+</button>
+</div>
+<div id=navbar class="navbar-collapse collapse">
+<ul class="nav navbar-nav">
+<li<?=$current?>><a href="<?=$url?>" class=home><span class="glyphicon glyphicon-home"></span> <?=$home?></a></li>
+<?=$nav?>
+</ul>
+</div>
+</div>
+</nav>
+<div class=container>
+<ol class=breadcrumb>
+<?=$breadcrumb?>
+</ol>
+<div class="row rear">
+<div class=col-md-9><?=$article?></div>
+<div class=col-md-3><?=$aside?></div>
+</div>
+</div>
+<div class="navbar navbar-inverse footer">
+<div class=container>
+<footer class=text-center><script src="<?=$js?>"></script>
+<?=$footer?>
+</footer>
+</div>
+</div>
+</body>
+</html>
---
+++ templates/bootstrap-navbar/js/index.php
@@ -0,0 +1,25 @@
+<?
+/*
+  * @copyright  Copyright (C) 2017 Gari-Hari LLC. All rights reserved.
+  * @license    GPL 3.0 or later; see LICENSE file for details.
+  */
+
+if ( is_file( $config = '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'config.php' ) && !is_link( $config ) ) include_once $config;
+
+$last_modified = timestamp( 'jquery.min.js' );
+
+header( 'Content-Type: application/javascript; charset=' . $encoding );
+
+header( 'Last-Modified: ' . $last_modified );
+
+if ( filter_input( INPUT_SERVER, 'HTTP_IF_MODIFIED_SINCE' ) === $last_modified ) header( 'HTTP/1.1 304 Not Modified' );
+
+echo
+
+file_get_contents( 'jquery.min.js' ),
+
+file_get_contents( 'bootstrap.min.js' ),
+
+file_get_contents( 'jquery.magnific-popup.min.js' ),
+
+'/* kinaga */$( document ).ready( function(){ $( "a[href=\"#TOP\"]" ).click( function(){ $( "body, html" ).animate( { scrollTop: 0 },100 ); return false } ); } ); $( ".gallery" ).each( function(){ $( this ).magnificPopup( { delegate: "a", type: "image", gallery: { enabled: true, preload: [1,1] } } ) } ); $( ".expand" ).magnificPopup( { type: "image" } ); $( ".discrete" ).magnificPopup( { type: "image" } ); $( ".nav-tabs a" ).click( function(e){ e.preventDefault(); $( this ).tab( "show" ) } )';

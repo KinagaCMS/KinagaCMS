d44aefaa54292d424f466e5634b12c0d26429c88
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2017-06-01 12:59:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,53 @@
 <?php
 /*
- * @copyright  Copyright (C) 2017 Gari-Hari LLC. All rights reserved.
- * @license  GPL 3.0 or later; see LICENSE file for details.
- */
+  * @copyright  Copyright (C) 2017 Gari-Hari LLC. All rights reserved.
+  * @license  GPL 3.0 or later; see LICENSE file for details.
+  */
 
-#exit( 'Down for Maintenance...' );
-#error_reporting( -1 );
+include_once 'includes' . DIRECTORY_SEPARATOR . 'config.php';
 
-if ( is_link( 'contents' ) )
+header( 'Content-Type: text/plain; charset=' . $encoding );
+
+$glob = glob( '{' . $glob_dir . 'index.html,contents' . $s . '*.html}', GLOB_BRACE + GLOB_NOSORT );
+
+if ( $glob )
 {
-	exit;
+	foreach( $glob as $files )
+	{
+		$sort[] = !is_link( $files ) && !is_link( dirname( $files ) ) && !is_link( dirname( dirname( $files ) ) ) ? filemtime( $files ) . '-~-' . $files : '';
+	}
+	$sort = array_filter( $sort );
+
+	rsort( $sort );
+
+	for( $i = 0, $c = count( $sort ); $i < $c; ++$i )
+	{
+		$part = explode( '-~-', $sort[$i] );
+
+		$categ = basename( dirname( dirname( $part[1] ) ) );
+
+		$title = basename( dirname( $part[1] ) );
+
+		if ( $title == 'contents' )
+		{
+			$page = basename( $part[1], '.html' );
+
+			if ( $page == 'index' )
+			{
+				echo $url . $n;
+			}
+			else
+			{
+				echo $url . rawurlencode( $page ) . $n;
+			}
+		}
+		else
+		{
+			echo $url . rawurlencode( $categ ) . $s . rawurlencode( $title ) . $n;
+		}
+	}
 }
-if ( is_file( $config = 'includes' . DIRECTORY_SEPARATOR . 'config.php' ) && ! is_link( $config ) )
+if ( $use_contact === true )
 {
-	include_once $config;
-}
-header( 'Content-Type: text/html; charset=' . $encoding );
-
-include_once 'includes' . $s . 'functions.php';
-
-if ( is_file( $tpl = $tpl_dir . 'index.php' ) && ! is_link( $tpl ) )
-{
-	include_once $tpl;
-}
-else
-{
-	include_once $includes . 'index.php';
+	echo $url . rawurlencode( $contact_us );
 }

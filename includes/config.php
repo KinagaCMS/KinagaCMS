6639c8989c8033bfce729b068a85a8f55a957f7b
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2017-09-14 11:29:48 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -201,7 +201,7 @@ $addslash = $dir != $s ? $s : '';
 
 $script = r( $dir ) . $addslash;
 
-$scheme = isset( $_SERVER['HTTP_X_SAKURA_FORWARDED_FOR'] ) || !empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'off' ? 'https://' : 'http://';
+$scheme = isset( $_SERVER['HTTP_X_SAKURA_FORWARDED_FOR'] ) || !empty($_SERVER['HTTPS']) ? 'https://' : 'http://';
 
 $url = ( $port === '80' ) ? $scheme . $server . $script : $scheme . $server . ':' . $port . $script;
 

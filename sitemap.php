f37580c61bc887473c7b1a2228533428478b848c
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2017-09-16 10:21:39 +0900

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,13 +1,6 @@
 <?php
-/*
-  * @copyright  Copyright (C) 2017 Gari-Hari LLC. All rights reserved.
-  * @license  GPL 3.0 or later; see LICENSE file for details.
-  */
-
 include_once 'includes' . DIRECTORY_SEPARATOR . 'config.php';
-
 header( 'Content-Type: text/plain; charset=' . $encoding );
-
 $glob = glob( '{' . $glob_dir . 'index.html,contents' . $s . '*.html}', GLOB_BRACE + GLOB_NOSORT );
 
 if ( $glob )
@@ -17,15 +10,11 @@ if ( $glob )
 		$sort[] = !is_link( $files ) && !is_link( dirname( $files ) ) && !is_link( dirname( dirname( $files ) ) ) ? filemtime( $files ) . '-~-' . $files : '';
 	}
 	$sort = array_filter( $sort );
-
 	rsort( $sort );
-
 	for( $i = 0, $c = count( $sort ); $i < $c; ++$i )
 	{
 		$part = explode( '-~-', $sort[$i] );
-
 		$categ = basename( dirname( dirname( $part[1] ) ) );
-
 		$title = basename( dirname( $part[1] ) );
 
 		if ( $title == 'contents' )

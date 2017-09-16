0d42d1ce29d49c4a428f2373ec80b6b4936ab0ad
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2017-09-16 10:19:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-/*
- * @copyright  Copyright (C) 2017 Gari-Hari LLC. All rights reserved.
- * @license  GPL 3.0 or later; see LICENSE file for details.
- */
-
 #exit( 'Down for Maintenance...' );
 #error_reporting( -1 );
-
 if ( is_link( 'contents' ) )
 {
 	exit;
@@ -16,9 +10,7 @@ if ( is_file( $config = 'includes' . DIRECTORY_SEPARATOR . 'config.php' ) && ! i
 	include_once $config;
 }
 header( 'Content-Type: text/html; charset=' . $encoding );
-
 include_once 'includes' . $s . 'functions.php';
-
 if ( is_file( $tpl = $tpl_dir . 'index.php' ) && ! is_link( $tpl ) )
 {
 	include_once $tpl;

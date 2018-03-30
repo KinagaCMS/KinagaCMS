60bdc44d89bcd6e76ba64d2a10945ab727c27576
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2018-03-30 11:48:22 +0900

    Update index.php

--- templates/bootstrap4/css/index.php
+++ templates/bootstrap4/css/index.php
@@ -15,68 +15,61 @@ file_get_contents('bootstrap.min.css'),
 file_get_contents('magnific-popup.min.css'),
 
 '
-body{font-family:"Droid Sans","Yu Gothic",YuGothic,"Hiragino Sans",sans-serif;color:#555}
-
-.article{font-size:large;line-height:1.8}
-
-.similar-article{height:2rem}
-
-.card-columns{column-count:2}
-
-a:hover img{opacity:0.8}
-
-.bg-success.text-success{background-color:#dff0d8!important;color:#3c763d!important}
-.bg-info.text-info{background-color:#d9edf7!important;color:#31708f!important}
-.bg-warning.text-warning{background-color:#fcf8e3!important;color:#8a6d3b!important}
-.bg-danger.text-danger{background-color:#f2dede!important;color:#a94442!important}
-::selection,.highlight,.comment:target{background-color:#d9edf7;color:#31708f}
-::-moz-selection,.highlight,.comment:target{background-color:#d9edf7;color:#31708f}
-
-.mfp-title{padding-bottom:3em}
-.wrap,.mfp-title{word-wrap:break-word;white-space:pre-wrap}
-
 #top{bottom:1em;position:fixed;right:2em;display:none;z-index:10}
-.page-top{border-bottom:thin dotted;position:relative;text-decoration:none!important}
+.article{font-size:large;line-height:1.8}
+.bg-danger.text-danger{background-color:#f2dede!important;color:#a94442!important}
+.bg-info.text-info{background-color:#d9edf7!important;color:#31708f!important}
+.bg-success.text-success{background-color:#dff0d8!important;color:#3c763d!important}
+.bg-warning.text-warning{background-color:#fcf8e3!important;color:#8a6d3b!important}
+.card-columns{column-count:2}
+.custom-file-label:after{content:"\0053c2\007167"}
+.mfp-title{padding-bottom:3em}
+.page-top:after{content:"'.$pagetop.'";position:absolute;opacity:0;right:-2em}
 .page-top:hover:after{opacity:1;right:0;transition:.3s linear}
 .page-top:not(:hover):after{opacity:0;transition:.3s}
-.page-top:after{content:"'.$pagetop.'";position:absolute;opacity:0;right:-2em}
-
-.custom-file-label:after{content:"\0053c2\007167"}
+.page-top{border-bottom:thin dotted;position:relative;text-decoration:none!important}
+.similar-article{height:2rem}
+.title{color:white}
+.wrap,.mfp-title{word-wrap:break-word;white-space:pre-wrap}
+::-moz-selection,.highlight,.comment:target{background-color:#d9edf7;color:#31708f}
+::selection,.highlight,.comment:target{background-color:#d9edf7;color:#31708f}
+a:hover img{opacity:0.8}
+body{font-family:"Droid Sans","Yu Gothic",YuGothic,"Hiragino Sans",sans-serif;color:#555}
 
 @media(max-width:767px){.card-columns{column-count:1}}
-
-.t{background-color:rgb(85, 172, 238);border-color:rgb(85, 172, 238)}.t:hover{background-color:rgba(85, 172, 238, .9);border-color:rgba(85, 172, 238, .1)}
-.g{background-color:rgb(221, 75, 57);border-color:rgb(221, 75, 57)}.g:hover{background-color:rgba(221, 75, 57, .9);border-color:rgba(221, 75, 57, .1)}
 .f{background-color:rgb(59, 89, 152);border-color:rgb(59, 89, 152)}.f:hover{background-color:rgba(59, 89, 152, .9);border-color:rgba(59, 89, 152, .1)}
+.g{background-color:rgb(221, 75, 57);border-color:rgb(221, 75, 57)}.g:hover{background-color:rgba(221, 75, 57, .9);border-color:rgba(221, 75, 57, .1)}
+.t{background-color:rgb(85, 172, 238);border-color:rgb(85, 172, 238)}.t:hover{background-color:rgba(85, 172, 238, .9);border-color:rgba(85, 172, 238, .1)}
 ';
 
 if ($use_auto_wrap === true)
 	echo '
-.article{word-wrap:break-word;white-space:pre-wrap}
+.article
+{word-wrap:break-word;white-space:pre-wrap}
 
-.article .navbar,
-.article .media,
-.article .input-group,
-.article table,
-.article .row,
-.article ul,
-.article li,
-.article ol,
-.article dl,
-.article .card,
+.article #accordion,
+.article .accordion,
 .article .alert,
 .article .btn-group,
-.article fieldset,
-.article .list-group,
-.article .form-group,
-.article .form-horizontal,
-.article .modal,
-.article .tab-content,
+.article .card,
+.article .carousel,
 .article .custom-control,
 .article .custom-file,
-.article .carousel,
-.article .accordion,
-.article #accordion
+.article .form-group,
+.article .form-horizontal,
+.article .input-group,
+.article .list-group,
+.article .media,
+.article .modal,
+.article .navbar,
+.article .row,
+.article .tab-content,
+.article dl,
+.article fieldset,
+.article li,
+.article ol,
+.article table,
+.article ul
 {white-space:normal!important}
 
 ';
@@ -84,29 +77,30 @@ else
 	echo '.page-top{padding-top:2rem!important;margin-bottom:2rem!important}';
 
 if ($color)
+{
 	echo '
-a,
-.btn-link,
-.page-link,
 .badge-light,
-.custom-select,
+.btn-link,
+.btn-outline-primary,
 .custom-file-label,
+.custom-select,
 .form-control:focus,
-.btn-outline-primary
+.page-link,
+a
 {color:'.hsla($color).'}
 
-body,
+.custom-file-label:after,
 .form-control,
 .input-group-text,
-.custom-file-label:after,
 .list-group-item-action,
-.list-group-item.active,
+.list-group-item-primary,
+.nav-tabs .nav-item.show .nav-link,
 .nav-tabs .nav-link.active,
-.nav-tabs .nav-item.show .nav-link
+body
 {color:'.hsla($color, -10, -10, .9).'}
 
-.text-muted,
-.breadcrumb-item+.breadcrumb-item:before
+.breadcrumb-item+.breadcrumb-item:before,
+.text-muted
 {color:'.hsla($color, -10, 3, .8).'!important}
 
 .blockquote-footer,
@@ -120,7 +114,7 @@ input.color
 {color:'.hsla($color, 5, 10, .8).'}
 
 .form-control::placeholder
-{color:'.hsla($color, '', '', .6).'}
+{color:'.hsla($color, 0, 0, .6).'}
 
 .nav-link.disabled
 {color:'.hsla($color, -5, -5, .5).'}
@@ -129,111 +123,138 @@ input.color
 .btn-outline-primary:hover
 {color:white!important}
 
-.btn-outline-primary:active:not(:disabled):not(.disabled),
-.btn-primary:hover,
-.dropdown-item:active,
-.nav-pills .show > .nav-link,
-.bg-primary
-{background-color:'.hsla($color).'!important}
-
 .badge-primary,
+.bg-primary,
+.btn-outline-primary:active:not(:disabled):not(.disabled),
 .btn-outline-primary:hover,
 .btn-primary,
+.dropdown-item:active,
 .nav-pills .nav-link.active,
-.navbar-dark.bg-primary,
+.nav-pills .show > .nav-link,
 .page-item.active .page-link,
 .progress-bar.bg-primary,
 .title
-{background-color:'.hsla($color, 0, 0, .9).'!important}
+{background-color:'.hsla($color).'!important}
 
+.btn-primary:hover,
+.list-group-item.active
+{background-color:'.hsla($color, -5, -5).'!important}
+
+.border-primary,
+.btn-outline-primary,
+.btn-outline-primary:hover,
+.btn-primary,
+.btn-primary:active:not(:disabled):not(.disabled),
+.list-group-item-primary,
+.page-item.active .page-link,
+.page-link
+{border-color:'.hsla($color).'!important}
+
+
+.btn-outline-primary,
+.btn-primary:hover,
+.card,
+.card-footer,
+.card-header,
+.custom-file-label,
+.custom-file-label:after,
+.custom-select,
+.form-control,
+.img-thumbnail,
+.input-group-text,
+.list-group-item,
+.list-group-item-primary,
+.nav-tabs .nav-item.show .nav-link,
+.nav-tabs,
+.page-link,
+.table td,
+.table th
+{border-color:'.hsla($color, -10, -15, .3).'!important}
+
+.nav-tabs
+{border-bottom:1px solid '.hsla($color, 0, 0, .3).'!important}
+
+.nav-tabs .nav-link.active
+{border-color:'.hsla($color, -5, 5, .2).' '.hsla($color, 0, 0, .2).' white!important}
+
+.nav-tabs .nav-link:hover
+{border-color:'.hsla($color, 0, 0, .1).'}
+
+.btn-outline-primary:active:focus:not(:disabled):not(.disabled),
+.btn-outline-primary:focus,
+.btn-primary.dropdown-toggle:focus,
+.btn-primary:active:focus:not(:disabled):not(.disabled),
+.btn-primary:active:not(:disabled):not(.disabled), .show > .btn-primary.dropdown-toggle,
+.btn-primary:focus,
+.custom-select:focus,
+.form-control:focus,
+.page-link:focus
+{box-shadow: 0 0 0 .2rem '.hsla($color, 5, 5, .2).'!important}
+
+::selection, .highlight, .comment:target{background-color:'.hsla($color, 0, 0, .2).';color:'.hsla($color).'}
+::-moz-selection, .highlight, .comment:target{background-color:'.hsla($color, 0, 0, .2).';color:'.hsla($color).'}
+';
+
+if (color2class($color) === 'dark')
+	echo '
 .breadcrumb,
+.bg-light,
+.card-footer,
+.list-group-item-action:focus,
+.list-group-item-primary,
+.progress
+{background-color:'.hsla($color, -10, 88).'!important}
+
 .card-header,
 .custom-file-label:after,
 .dropdown-item:hover,
 .input-group-text,
 .jumbotron,
-.list-group-item-action:focus,
-.list-group-item-action:focus,
 .list-group-item-action:hover,
-.list-group-item.active,
 .page-link:hover,
-.progress,
-.table-active, .table-active > td, .table-active > th,
+.table-primary,
 a.bg-light:focus,
 a.bg-light:hover,
-a.bg-primary:hover,
-button.bg-light:focus,
-button.bg-light:hover
-{background-color:'.hsla($color, -40, 30, .2).'!important}
-
-.bg-light,
-.card-footer
-{background-color:'.hsla($color, 0, 30, .1).'!important}
-
-.border-primary,
-.list-group-item.active,
-.page-link,
-.page-item.active .page-link,
-.btn-outline-primary,
-.btn-outline-primary:hover,
-.btn-primary,
-.btn-primary:active:not(:disabled):not(.disabled)
-{border-color:'.hsla($color).'!important}
-
-.list-group-item,
-.card,
-.form-control,
-.card-header,
-.table td,
-.table th,
-.nav-tabs,
-.nav-tabs .nav-item.show .nav-link,
-.img-thumbnail,
-.card-footer,
-.list-group-item.active,
-.input-group-text,
-.page-link,
-.btn-outline-primary,
-.btn-primary:hover,
-.page-link,
-.custom-select,
-.custom-file-label,
-.custom-file-label:after
-{border-color:'.hsla($color, -10, -15, .3).'!important}
-
-.nav-tabs
-{border-bottom:1px solid '.hsla($color, '', '', .3).'!important}
-
-.nav-tabs .nav-link.active
-{border-color:'.hsla($color, -5, 5, .2).' '.hsla($color, '', '', .2).' white!important}
-
-.nav-tabs .nav-link:hover
-{border-color:'.hsla($color, '', '', .1).'}
-
-.page-link:focus,
-.form-control:focus,
-.custom-select:focus,
-.btn-primary:focus,
-.btn-primary:active:focus:not(:disabled):not(.disabled),
-.btn-outline-primary:focus,
-.btn-outline-primary:active:focus:not(:disabled):not(.disabled),
-.btn-primary.dropdown-toggle:focus,
-.btn-primary:active:not(:disabled):not(.disabled), .show > .btn-primary.dropdown-toggle
-{box-shadow: 0 0 0 .2rem '.hsla($color, 5, 5, .2).'!important}
-
-
-::selection, .highlight, .comment:target{background-color:'.hsla($color, '', '', .2).';color:'.hsla($color).'}
-::-moz-selection, .highlight, .comment:target{background-color:'.hsla($color, '', '', .2).';color:'.hsla($color).'}
+a.bg-primary:focus,
+a.bg-primary:hover
+{background-color:'.hsla($color, -40, 80).'!important}
 
 ';
+else
+	echo '
+.breadcrumb,
+.bg-light,
+.card-footer,
+.list-group-item-action:focus,
+.list-group-item-primary,
+.progress
+{background-color:'.hsla($color, -5, 55).'!important}
+
+.card-header,
+.custom-file-label:after,
+.dropdown-item:hover,
+.input-group-text,
+.jumbotron,
+.table-primary, .table-primary > td, .table-primary > th,
+.list-group-item-action:hover,
+.page-link:hover,
+a.bg-light:focus,
+a.bg-light:hover,
+a.bg-primary:focus,
+a.bg-primary:hover
+{background-color:'.hsla($color, -15, 55).'!important}
+
+';
+
 if (color2class($color) === 'white')
 	echo '
+.list-group-item-primary,
 .navbar-dark .navbar-brand,
 .navbar-dark .navbar-nav .active > .nav-link,
 .navbar-dark .navbar-nav .nav-link.active,
 .navbar-dark .navbar-nav .nav-link.show,
 .navbar-dark .navbar-nav .show > .nav-link,
-.list-group-item.active
+.title
 {color:'.hsla($color).'!important}
 
 .navbar-dark .navbar-nav .nav-link
@@ -243,7 +264,7 @@ if (color2class($color) === 'white')
 {color:'.hsla($color, 3, 3, .5).'!important}
 
 .navbar-dark .navbar-toggler
-{border-color:'.hsla($color).';background-color:'.hsla($color).'}
+{border-color:'.hsla($color).'}
 
 .navbar-dark .navbar-toggler:hover
 {opacity:0.5}
@@ -252,34 +273,29 @@ if (color2class($color) === 'white')
 {background-image:url("data:image/svg+xml;charset=utf8,%3Csvg viewBox=\'0 0 30 30\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cpath stroke=\''.hsla($color).'\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-miterlimit=\'10\' d=\'M4 7h22M4 15h22M4 23h22\'/%3E%3C/svg%3E")!important}
 
 .breadcrumb,
-.col-lg-3 .card-header.bg-light,
-.col-lg-3 .list-group-item.active,
-.col-lg-3 .list-group-item.bg-light,
+.card-header,
+.list-group-item-primary,
 .navbar-dark.bg-primary,
+.table-primary, .table-primary > td, .table-primary > th,
 .title
 {background-color:white!important}
 
-.col-lg-3 .list-group-item,
-.col-lg-3 .card
+.col-lg-3 .card,
+.col-lg-3 .list-group-item
 {border-left:0;border-right:0;border-radius:0}
 
-.col-lg-3 .list-group-item:first-child,
-.col-lg-3 .card
+.col-lg-3 .card,
+.col-lg-3 .list-group-item:first-child
 {border-top:0}
 
-body,
+.custom-file-label:after,
 .form-control,
 .input-group-text,
-.custom-file-label:after,
 .list-group-item-action,
-.list-group-item.active,
+.nav-tabs .nav-item.show .nav-link,
 .nav-tabs .nav-link.active,
-.nav-tabs .nav-item.show .nav-link
+body
 {color:'.hsla($color, 0, -10, .9).'}
 
 ';
-else
-	echo '
-.title{color:white}
-
-';
+}

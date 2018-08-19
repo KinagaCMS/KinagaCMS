3673af92aacd604d9dc3e2bfef1aa8523764dcc7
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2018-08-19 16:18:28 +0900

    txt -> xml

--- sitemap.php
+++ sitemap.php
@@ -1,6 +1,12 @@
 <?php
 include_once 'includes/config.php';
-header('Content-Type: text/plain; charset=' . $encoding);
+header('Content-Type: application/xml; charset=' . $encoding);
+$xml = new DOMDocument('1.0', $encoding);
+$insert = $xml->firstChild;
+$style = $xml->createProcessingInstruction('xml-stylesheet', 'type="text/css" href="'. $css. '"');
+$xml->insertBefore($style, $insert);
+echo $xml->saveXML(),
+'<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9/">', $n;
 $glob = glob('{' . $glob_dir . 'index.html,contents/*.html}', GLOB_BRACE + GLOB_NOSORT);
 
 if ($glob)
@@ -21,13 +27,15 @@ if ($glob)
 			$page = basename($part[1], '.html');
 
 			if ($page == 'index')
-				echo $url . $n;
+				echo '<url><loc>', $url, '</loc></url>', $n;
 			else
-				echo $url . rawurlencode($page) . $n;
+				echo '<url><loc>', $url, rawurlencode($page), '</loc><lastmod>', date('Y-m-d\TH:i:s', $part[0]), '</lastmod></url>', $n;
 		}
 		else
-			echo $url . rawurlencode($categ) . '/' . rawurlencode($title) . $n;
+			echo '<url><loc>', $url, rawurlencode($categ), '/', rawurlencode($title), '</loc><lastmod>', date('Y-m-d\TH:i:s', $part[0]), '</lastmod></url>', $n;
 	}
 }
 if ($use_contact === true)
-	echo $url . rawurlencode($contact_us);
+	echo '<url><loc>', $url, rawurlencode($contact_us), '</loc></url>', $n;
+
+echo '</urlset>';

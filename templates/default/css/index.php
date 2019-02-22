b617483b269a5196a8e3fb37983455f9e36123cc
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2019-02-22 15:09:19 +0900

    Update index.php

--- templates/default/css/index.php
+++ templates/default/css/index.php
@@ -58,6 +58,7 @@ file_get_contents('magnific-popup.min.css'), '
 .popover{max-width:70%}
 .similar-article{height:2rem}
 .sticky-top{top:10px}
+.text-light{color:'. ($color ? hsla($color, -5, +20, .9) : 'inherit'). ' !Important}
 .title{color:white}
 .wrap,.mfp-title,.popover-body{word-wrap:break-word;white-space:pre-wrap}
 ::-moz-selection,.highlight,.comment:target{background-color:#d9edf7;color:#31708f}

f8e5bc9622f271e511f02de699a63f8818cb23f7
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2019-03-14 11:09:25 +0900

    Update index.php

--- templates/default/index.php
+++ templates/default/index.php
@@ -25,7 +25,7 @@
 </div>
 </div>
 <footer class="text-center pb-3">
-<a href="#TOP" id=page-top class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="1em"><path  fill="currentColor" d="m2.5872 45.447-2.5872-2.043 24.034-40.851 23.966 40.987-2.996 1.362-21.038-35.745z"/></svg></a>
+<a href="#TOP" id=page-top class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path  fill="currentColor" d="m2.5872 45.447-2.5872-2.043 24.034-40.851 23.966 40.987-2.996 1.362-21.038-35.745z"/></svg></a>
 <script src="<?=$js?>"></script>
 <?=$footer?>
 </footer>

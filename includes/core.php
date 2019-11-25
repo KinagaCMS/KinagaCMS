27c8919a620ed09eaf7bad2dadfb108df6cbe957
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2019-11-25 14:25:10 +0900

    Update core.php

--- includes/core.php
+++ includes/core.php
@@ -22,6 +22,26 @@ $session_f = !filter_has_var(INPUT_POST, 'f') ? '' : filter_input(INPUT_POST, 'f
 $user = !filter_has_var(INPUT_GET, 'user') ? '' : filter_input(INPUT_GET, 'user', FILTER_SANITIZE_STRING);
 $session_t = !filter_has_var(INPUT_POST, 't') ? '' : filter_input(INPUT_POST, 't', FILTER_SANITIZE_STRING);
 
+if (!extension_loaded('imagick'))
+{
+	$imagick_so = 'extension=imagick.so';
+	if (is_file($php_ini = $_SERVER['DOCUMENT_ROOT']. '/php.ini'))
+	{
+		$pos_imagick_so = strpos(file_get_contents($php_ini), $imagick_so);
+		if ($pos_imagick_so === false)
+			file_put_contents($php_ini, $imagick_so. $n, FILE_APPEND | LOCK_EX);
+		else
+			file_put_contents('./error.log', 'ImageMagick is not installed. See https://www.php.net/manual/'. $lang. '/imagick.setup.php'. $n, LOCK_EX);
+	}
+	else
+		file_put_contents($php_ini, $imagick_so. $n, LOCK_EX);
+}
+else
+{
+	if ($pos_imagick_so === false)
+		file_put_contents($php_ini, $imagick_so. $n, FILE_APPEND | LOCK_EX);
+}
+
 if (is_file($conf = $tpl_dir. 'config.php')) include $conf;
 
 include 'session.php';

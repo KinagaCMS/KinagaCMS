ae3c8a62bdce587927dba47dcd235737bd4c9b57
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2020-03-25 11:59:18 +0900

    Update index.php

--- templates/default/index.php
+++ templates/default/index.php
@@ -3,31 +3,25 @@
 <head>
 <meta charset=<?=$encoding?>>
 <meta name=viewport content="width=device-width,initial-scale=1">
-<link href="<?=$css, ($get_categ ? '?categ='.$get_categ : '')?>" rel=stylesheet>
+<link href="<?=$css?>" rel=stylesheet>
 <?=$header?>
 </head>
 <body id=TOP>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary title">
-<a class=navbar-brand href="<?=$url?>"><?=get_logo()?></a>
-<button class=navbar-toggler type=button data-toggle=collapse data-target="#nav" accesskey=n tabindex=100>
-<span class=navbar-toggler-icon></span>
-</button>
-<div class="collapse navbar-collapse" id=nav>
-<ul class="navbar-nav mr-auto mt-2 mt-lg-0"><?=$nav?></ul>
-<?=$search?>
-</div>
-</nav>
-<ol class="breadcrumb rounded-0 justify-content-end"><?=$breadcrumb?></ol>
-<div class=container-fluid>
-<div class=row>
-<div id=main class=col-lg-9><?=$article?></div>
-<div id=side class="col-lg-3 d-flex flex-column"><?=$aside?></div>
+<div class=container>
+<header class="py-3 row justify-content-between align-items-center">
+<a class="col-md-2 text-md-left text-center" href="<?=$url?>atom.php"><img src="<?=$css?>Rss_font_awesome.svg" data-license="Dave Gandy CC BY-SA 3.0" alt=atom width=25 height=25></a>
+<h1 class="col-md-8 text-center h2"><a class="text-white" href="<?=$url?>"><?=get_logo()?></a></h1>
+<div class="col-md-2 text-md-right"><?=$search?></div>
+</header>
+<nav><ul class="list-unstyled py-2"><?=$nav?></ul></nav>
+<div class="row">
+<main id=main class="col-lg-8 mt-4"><?=$article?></main>
+<aside id=side class="col-lg-4 d-flex flex-column"><?=$aside?></aside>
 </div>
 </div>
-<footer class="text-center pb-3">
-<a href="#TOP" id=page-top class="btn btn-outline-primary">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="currentColor" d="m2.5872 45.447-2.5872-2.043 24.034-40.851 23.966 40.987-2.996 1.362-21.038-35.745z"/></svg>
-</a>
+<footer class="text-center pb-2 pt-4">
+<ol class="breadcrumb bg-transparent justify-content-center"><?=$breadcrumb?></ol>
+<a href="#TOP" id=page-top class="btn btn-outline-dark"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="currentColor" d="m2.5872 45.447-2.5872-2.043 24.034-40.851 23.966 40.987-2.996 1.362-21.038-35.745z"/></svg></a>
 <script src="<?=$js?>"></script>
 <?=$footer?>
 </footer>

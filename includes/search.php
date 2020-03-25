5507813444efed2d84d86ea42278e924b3e6421e
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2020-03-25 11:55:28 +0900

    Update search.php

--- includes/search.php
+++ includes/search.php
@@ -14,11 +14,11 @@ if ($query)
 
 		foreach($glob_search as $filename)
 		{
-			$temp = h(file_get_contents($filename));
+			$temp = file_get_contents($filename);
 			$file_title = get_title($filename);
 			$temp_title = $file_title === 'index' ? strip_tags(basename($filename, '.html')) : get_categ($filename). $file_title;
 			$timestamp = date($time_format, filemtime($filename));
-			$first_pos = mb_stripos($temp. $temp_title, $query);
+			$first_pos = mb_stripos($temp. $temp_title. $timestamp, html_entity_decode($query));
 
 			if ($first_pos !== false)
 			{
@@ -47,24 +47,24 @@ if ($query)
 				$output_categ = get_categ($output[1]);
 				$pagename = basename($output[1], '.html');
 
-				$article .= '<section class="p-2 mb-5 border-bottom">'. $n;
+				$article .= '<section class="p-2 mb-5 border-bottom position-relative">'. $n;
 
 				if ($output_title === 'index')
 					$article .=
-					'<h2 class=h4><a href="'. $url. '">'. $home. '</a></h2>'. $n.
-					'<div class="wrap p-2">'. $output[2]. '</div>'. $n.
+					'<h2 class=h4><a class=stretched-link href="'. $url. '">'. $home. '</a></h2>'. $n.
+					'<div class="wrap p-2">'. h($output[2]). '</div>'. $n.
 					'<small class="blockquote-footer text-right">'. $output[0].'</small>';
 
 				elseif ($pagename !== 'index')
 					$article .=
-					'<h2 class=h4><a href="'. $url. r($pagename). '">'. h($pagename). '</a></h2>'. $n.
-					'<div class="wrap p-2">'. $output[2]. '</div>'. $n.
+					'<h2 class=h4><a class=stretched-link href="'. $url. r($pagename). '">'. h($pagename). '</a></h2>'. $n.
+					'<div class="wrap p-2">'. h($output[2]). '</div>'. $n.
 					'<small class="blockquote-footer text-right">'. $output[0].'</small>';
 
 				else
 					$article .=
-					'<h2 class=h4><a href="'. $url. r($output_categ. '/'. $output_title). '">'. h($output_title). '</a></h2>'. $n.
-					'<div class="wrap p-2">'. $output[2]. '</div>'. $n.
+					'<h2 class=h4><a class=stretched-link href="'. $url. r($output_categ. '/'. $output_title). '">'. h($output_title). '</a></h2>'. $n.
+					'<div class="wrap p-2">'. h($output[2]). '</div>'. $n.
 					'<small class="blockquote-footer text-right">'. $output[0]. ' - '. h($output_categ). '</small>';
 
 				$article .= '</section>'. $n;

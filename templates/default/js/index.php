7084abf6c6313680b9e16cdcfc0d5570ebdad060
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2020-03-25 12:05:07 +0900

    Update index.php

--- templates/default/js/index.php
+++ templates/default/js/index.php
@@ -1,12 +1,10 @@
 <?php
-header('Content-Type: application/javascript');
-header('Last-Modified: '. $last_modified = gmdate('D, d M Y H:i:s T', filemtime(__FILE__)));
-
+header('Last-Modified: '. $last_modified = gmdate('D, d M Y H:i:s T', getlastmod()));
 if (filter_input(INPUT_SERVER, 'HTTP_IF_MODIFIED_SINCE') === $last_modified) header('HTTP/1.1 304 Not Modified');
-
+header('Content-Type: text/javascript');
 echo
 file_get_contents('jquery.min.js'),
 file_get_contents('popper.min.js'),PHP_EOL,
 file_get_contents('bootstrap.min.js'),PHP_EOL,
-file_get_contents('jquery.fancybox.min.js');?>
-$('a[href="#TOP"]').click(function(){$('body, html').animate({scrollTop:0},100);return false});let scroll_delay=0;$(window).on('scroll',function(){clearTimeout(scroll_delay);scroll_delay=setTimeout(function(){if($(this).scrollTop()>200){$('#page-top').slideDown()}else{$('#page-top').slideUp()}},400)});$('.nav-tabs a').click(function(e){e.preventDefault();$(this).tab('show')});$('[data-toggle="tooltip"]').tooltip();$('[data-toggle="popover"]').popover();
+file_get_contents('jquery.fancybox.min.js')?>
+$('a[href="#TOP"]').click(function(){$('body, html').animate({scrollTop:0},100);return false});let scroll_delay=0;$(window).on('scroll',function(){clearTimeout(scroll_delay);scroll_delay=setTimeout(function(){if($(this).scrollTop()>200){$('#page-top').slideDown()}else{$('#page-top').slideUp()}},400)});$('.nav-tabs a').click(function(e){e.preventDefault();$(this).tab('show')});$('[data-toggle="tooltip"]').tooltip();$('[data-toggle="popover"]').popover();$('img').onerror=null;
